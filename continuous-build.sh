6261c1c038c996021cd3d00cc00b5639bffd2e81
Владимир Лазарев <wolodyx@yandex.ru>
2024-02-15 10:01:58 +0300

    Generate images from excalidraw files

--- continuous-build.sh
+++ continuous-build.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-rm -rf _build
-jupyter-book build .
-
-while inotifywait -r -e modify ./src intro.md _toc.yml _config.yml; do
+function BuildBook() {
+  excalidraw-cli
   jupyter-book build .
+  for filename in $(ls -1 ./src/images/*.excalidraw); do filename="${filename%.excalidraw}"; rm -f $filename.png; done
+}
+
+rm -rf _build
+BuildBook
+while inotifywait -r -e modify ./src intro.md _toc.yml _config.yml; do
+  BuildBook
 done
 
