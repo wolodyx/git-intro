db3b992f755274e58215f23ba8a0d63d8d8379e6
Владимир Лазарев <wolodyx@yandex.ru>
2024-05-06 10:55:15 +0300

    +

---
+++ examples/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get -y install git-all gitk
+sudo apt -y install vim tmux rcs git-gui curl tree
+sudo apt -y install kdiff3
+
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+
