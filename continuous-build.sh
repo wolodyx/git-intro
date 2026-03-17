be0aec8f5b9e56217915f4688b41033e76207a63
Владимир Лазарев <11465238+wolodyx@users.noreply.github.com>
2026-03-17 20:56:12 +0300

    Update jupyter-book version

--- continuous-build.sh
+++ continuous-build.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-
-function BuildBook() {
-  excalidraw-cli
-  jupyter-book build .
-  for filename in $(ls -1 ./src/images/*.excalidraw); do filename="${filename%.excalidraw}"; rm -f $filename.png; done
-}
+set -e
 
 rm -rf _build
-BuildBook
-while inotifywait -r -e modify ./src intro.md _toc.yml _config.yml; do
-  BuildBook
+for filename in $(ls -1 ./src/images/*.excalidraw); do filename="${filename%.excalidraw}"; rm -f $filename.png; done
+excalidraw-cli
+jupyter-book start &
+while inotifywait -r -e modify ./myst.yml; do
+  pid=$(jobs -p %1)
+  kill ${pid}
+  rm -rf _build
+  for filename in $(ls -1 ./src/images/*.excalidraw); do filename="${filename%.excalidraw}"; rm -f $filename.png; done
+  excalidraw-cli
+  jupyter-book start &
 done
-
